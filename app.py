253f5064436918ceab6a570fd66e65da055df83b
Muhammad Teguh Prastyo <128394669+teguhprastyopy@users.noreply.github.com>
2023-12-27 19:35:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,79 +1,53 @@
 import streamlit as st 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.chains.summarize import load_summarize_chain
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-from transformers import pipeline
+from transformers import T5Tokenizer, T5ForConditionalGeneration, pipeline
 import torch
 import base64
 
 #model and tokenizer loading
-checkpoint = "MBZUAI/LaMini-Flan-T5-248M"
-tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, torch_dtype=torch.float32)
+@st.cache(allow_output_mutation=True)
+def load_model():
+    checkpoint = "MBZUAI/LaMini-Flan-T5-248M"
+    tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+    base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, torch_dtype=torch.float32)
+    return tokenizer, base_model
 
-#file loader and preprocessing
-def file_preprocessing(file):
-    loader =  PyPDFLoader(file)
-    pages = loader.load_and_split()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
-    texts = text_splitter.split_documents(pages)
-    final_texts = ""
-    for text in texts:
-        print(text)
-        final_texts = final_texts + text.page_content
-    return final_texts
+tokenizer, base_model = load_model()
 
-#LLM pipeline
-def llm_pipeline(filepath):
-    pipe_sum = pipeline(
-        'summarization',
-        model = base_model,
-        tokenizer = tokenizer,
-        max_length = 500, 
-        min_length = 50)
-    input_text = file_preprocessing(filepath)
+# Function to process PDF file and return text
+def process_pdf(uploaded_file):
+    # Code to process PDF file and return text
+    # ...
+    return processed_text
+
+# LLM pipeline
+def llm_pipeline(input_text):
+    pipe_sum = pipeline('summarization', model=base_model, tokenizer=tokenizer, max_length=500, min_length=50)
     result = pipe_sum(input_text)
-    result = result[0]['summary_text']
-    return result
+    return result[0]['summary_text']
 
-@st.cache_data
-#function to display the PDF of a given file 
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # Embedding PDF in HTML
+# Function to display PDF
+def display_pdf(uploaded_file):
+    base64_pdf = base64.b64encode(uploaded_file.getvalue()).decode('utf-8')
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
-
-    # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-#streamlit code 
-st.set_page_config(layout="wide")
-
+# Streamlit UI code
 def main():
     st.title("Document Summarization App")
-
     uploaded_file = st.file_uploader("Upload your PDF file", type=['pdf'])
 
     if uploaded_file is not None:
         if st.button("Summarize"):
             col1, col2 = st.columns(2)
-            filepath = "data/"+uploaded_file.name
-            with open(filepath, "wb") as temp_file:
-                temp_file.write(uploaded_file.read())
             with col1:
                 st.info("Uploaded File")
-                pdf_view = displayPDF(filepath)
+                display_pdf(uploaded_file)
 
             with col2:
-                summary = llm_pipeline(filepath)
+                processed_text = process_pdf(uploaded_file)
+                summary = llm_pipeline(processed_text)
                 st.info("Summarization Complete")
                 st.success(summary)
-
-
 
 if __name__ == "__main__":
     main()
